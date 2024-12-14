af4d10e9b72f1964b8b7c3ec57728a3700698f88
AnushiyaG14 <anushiya2014@gmail.com>
2024-12-14 16:15:12 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -122,31 +122,5 @@ else:
         except Exception as e:
              st.error(f"Error executing query: {e}")
 
-### Sidebar for Navigation
-st.sidebar.title("Business insights of Retail Order Data Analysis  !")
-page = st.sidebar.selectbox("Go to", ["Home", "Top Selling Products", "Year over Year sales Analysis", "Product Performance","Regional Sales Analysis","Discount Analysis"])
-
-# Main App Logic###
-if page == "Home":
-    st.title("Business insights of Retail Order Data Analysis  !")
-    
-elif page == "Top Selling Products":
-    st.title("Top Selling Products")
-    st.image("D:\\streamlit\\env\\images\\Capture1.PNG","Caption = Top 10 Revenue generating products")
-    
-elif page == "Year over Year sales Analysis":
-    st.title("Year over Year sales Analysis")
-    st.image("D:\\streamlit\\env\\images\\C2.png", "Caption = Yearly Sales Analysis")
-    st.image("D:\\streamlit\\env\\images\\Capture2.PNG","Caption = Monthly sales Analysis ")
-elif page == "Product Performance":
-    st.title("Product Performance")
-    st.image("D:\\streamlit\\env\\images\\Capture1.PNG", "caption = Product rank by Revenue basis")
-    
-elif page == "Regional Sales Analysis":
-    st.title("Regional Sales Analysis")
-    st.image("D:\\streamlit\\env\\images\\Capture3.png","caption =Regional Sales Analysis ")
-elif page == "Discount Analysis":
-    st.title("Discount Analysis")
-    st.image("D:\\streamlit\\env\\images\\Capture5.PNG","caption = Product with greater than 20 % disscount ")
 
 st.text("Thank you")
